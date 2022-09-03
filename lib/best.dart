29993d029198f8eaef4554e3c1890bab4cea4801
Saad Ali <msaadali602@gmail.com>
2022-09-03 11:54:41 +0100

    Sample Project

---
+++ lib/best.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter FlatButton Example'),
+          ),
+          body: Center(child: Column(children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(25),
+              child: TextButton(
+                child: Text('SignUp', style: TextStyle(fontSize: 20.0),),
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(25),
+              child: TextButton(
+                child: Text('LogIn', style: TextStyle(fontSize: 20.0,color: Colors.white)),
+                onPressed: () {},
+              ),
+              color: Colors.black,
+            ),
+          ]
+          ))
+      ),
+    );
+  }
+}
